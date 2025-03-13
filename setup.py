da4f726524974ec31c2a496f62f57429916b8b98
Jose F. Velez <jose.velez@urjc.es>
2025-03-13 16:18:58 +0100

    Creating project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+name='page_limits',
+version='1.0',
+description='Package to detect page limits in a page picture of a page.',
+author='Jose Velez',
+author_email='jose.velez@urjc.es',
+packages=['page_limits'],
+install_requires=['opencv-python']
+)
