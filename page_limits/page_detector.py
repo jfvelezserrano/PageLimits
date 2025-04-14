b61a8a1e7924eef66c516ad38861f6f45fc74646
Jose F. Velez <jose.velez@urjc.es>
2025-04-14 16:44:53 +0200

    Solving errors

--- page_limits/page_detector.py
+++ page_limits/page_detector.py
@@ -15,6 +15,9 @@ class Preprocesor:
     
 class OtsuPreprocesor(Preprocesor):
     
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+    
     def __binarize_image(self, image):
         _,binary_image = cv2.threshold(image,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
         return binary_image
@@ -35,6 +38,9 @@ class OtsuPreprocesor(Preprocesor):
         return image
 
 class HistPreprocesor(Preprocesor):
+    
+    def __init__(self, verbose = False):
+        self.verbose = verbose
     
     def __binarize_image(self, image):
         hist_item = cv2.calcHist([image],[0],None,[256],[0,255])
@@ -80,13 +86,16 @@ class HistPreprocesor(Preprocesor):
         return image
         
 class AdaptativePreprocesor(Preprocesor):
-    def __binarize_image(self, image,verbose = False):
+    def __binarize_image(self, image):
         binary_image = cv2.adaptiveThreshold(image,255,cv2.BORDER_REPLICATE,cv2.THRESH_BINARY,31,5)
         return binary_image
 
     def process(self,image):
         image = self.__binarize_image(image)
         return image
+    
+    def __init__(self, verbose=False):
+        self.verbose = verbose
 
 class PointDetector:
     pass
@@ -122,13 +131,17 @@ class PointFilter:
     def __init__(self, verbose = False):
         self.verbose = verbose
         self.KEYPOINT_SIZE = 31
+        self.RESIZE = 4
     
     def filter(self,points):
         pass
+    
+    def get_resize_factor(self):
+        return self.RESIZE
 
 
 class SiftPointFilter(PointFilter):
-
+    
     def __str_2_tuple(self, cadena):
         # Eliminar espacios en blanco y saltos de línea
         cadena = cadena.strip()
@@ -162,12 +175,11 @@ class SiftPointFilter(PointFilter):
         """
             Por cada linea en samples
             Carga la imagen
-            La preprocesa: binarización y apertura
+            La preprocesa
             Calcula los descriptores
             Almacena los descriptores
         """
 
-        sift = cv2.xfeatures2d.SIFT_create()
         reference_descriptors = []
 
         for image_gt in samples:
@@ -188,35 +200,34 @@ class SiftPointFilter(PointFilter):
                 plt.scatter(corner_matrix[:, 0], corner_matrix[:, 1], c='red', marker="+")
 
             corner_keypoints = [cv2.KeyPoint(float(x[0]), float(x[1]), self.KEYPOINT_SIZE) for x in corner_matrix]
-            corner_descriptors = sift.compute(preprocess_image,corner_keypoints)
+            corner_descriptors = self.sift.compute(preprocess_image,corner_keypoints)
                         
             if self.verbose:
                 print(corner_descriptors[1].shape)
                 plt.show()
+                
+            reference_descriptors.append(corner_descriptors[1])
                             
-        return corner_descriptors[1]    
+        reference_descriptors = np.array(reference_descriptors)
+        num_images, corners, features = reference_descriptors.shape
+        reference_descriptors = reference_descriptors.reshape(num_images * corners, features)
+        return reference_descriptors    
     
     def __init__(self, preprocesor:Preprocesor, verbose = False):
         super().__init__(verbose)
-        self.RESIZE = 4
         self.preprocesor = preprocesor
+        self.sift = cv2.xfeatures2d.SIFT_create()
 
         samples = self.__read_samples()
         self.reference_descriptors = self.__compute_refence_descriptors(samples)
+        self.bf = cv2.BFMatcher(cv2.NORM_L2)
         
-    def filter(self,points, image):
-        image = cv2.resize(image,(image.shape[1]//self.RESIZE,image.shape[0]//self.RESIZE))        
+    def filter(self,points, image, verbose=False):
         harris_keypoints = [cv2.KeyPoint(float(x[0]), float(x[1]), self.KEYPOINT_SIZE) for x in points]
-        
-        sift = cv2.xfeatures2d.SIFT_create()
-        
-        query_descriptors = sift.compute(image,harris_keypoints)
+        query_descriptors = self.sift.compute(image,harris_keypoints)
 
         for cont in range(0,query_descriptors[1].shape[0]):
-        
-            bf = cv2.BFMatcher(cv2.NORM_L2)
-                
-            matches = bf.knnMatch(query_descriptors[1], self.reference_descriptors, k=1) 
+            matches = self.bf.knnMatch(query_descriptors[1], self.reference_descriptors, k=1) 
             
             height = image.shape[0]
             width = image.shape[1]
@@ -245,27 +256,32 @@ class SiftPointFilter(PointFilter):
             
             output = (best_matches_Q1,best_matches_Q2,best_matches_Q3,best_matches_Q4)
             output = np.array(output)
+            output = output[:,1:3].astype(np.uint32)
             
-            plt.subplot(1,5,5)
-            plt.imshow(image,cmap="gray")
-            plt.scatter(output[:, 1], output[:, 2], c='red', marker="+")
-            plt.show()
+            if True:#verbose:
+                plt.imshow(image,cmap="gray")
+                plt.scatter(output[:, 0], output[:, 1], c='red', marker="+")
+                plt.show()
             
             return output    
 
 class PageDetector:
-    def __init__(self,preprocesor:Preprocesor, pointDetector:PointDetector, pointFilter:PointFilter):
+    def __init__(self,preprocesor:Preprocesor, pointDetector:PointDetector, pointFilter:PointFilter, verbose=False):
         self.preprocesor = preprocesor
         self.pointDetector = pointDetector
         self.pointFilter = pointFilter
+        self.verbose = verbose
     
-    def __init__(self):
-        self.preprocesor = AdaptativePreprocesor()
-        self.pointDetector = HarrisPointDetector()
-        self.pointFilter = SiftPointFilter(self.preprocesor)
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+        self.preprocesor = AdaptativePreprocesor(self.verbose)
+        self.pointDetector = HarrisPointDetector(self.verbose)
+        self.pointFilter = SiftPointFilter(self.preprocesor, self.verbose)
 
     def detect(self, image:np.ndarray):
+        RESIZE_FACTOR = self.pointFilter.get_resize_factor()
+        image = cv2.resize(image,(image.shape[1]//RESIZE_FACTOR,image.shape[0]//RESIZE_FACTOR))
         preprocessed_image = self.preprocesor.process(image)
         points = self.pointDetector.detect(preprocessed_image)
         filtered_points = self.pointFilter.filter(points, image)        
-        return filtered_points
+        return filtered_points * self.pointFilter.get_resize_factor()
--- tests/test_page_detector.py
+++ tests/test_page_detector.py
@@ -1,16 +1,33 @@
 import unittest
 
 import cv2
+import numpy as np
 from page_limits.page_detector import PageDetector
 
 class TestPageDetector(unittest.TestCase):
     
+    
+    
+    def compare_corners(self, corner_list_1, corner_list_2):
+        detected_corners = set()
+        for corner_1 in corner_list_1:
+            best_corner = None
+            for corner_2 in corner_list_2:
+                if np.linalg.norm(corner_1 - corner_2) < 100:
+                    best_corner = corner_2
+            detected_corners.add(tuple(best_corner))
+        self.assertEqual(len(detected_corners),4)
+    
+    
     def test_page_detection(self):
-        p = PageDetector()
-        image = cv2.imread("samples/learning/Hoja_03.jpg",0)
+        p = PageDetector(False)
+        image = cv2.imread("samples/learning/Hoja_02.jpg",0)
         result = p.detect(image)
-        expected_result = [(195, 164), (484, 172), (210, 888), (506, 875)]
-        self.assertEqual(result,expected_result)
+
+        expected_result = [[211,351],[2565,452],[195,3751],[2574,3670]]
+        expected_result = np.array(expected_result)
+
+        self.compare_corners(result,expected_result)
         
 if __name__ == '__main__':
     unittest.main()
